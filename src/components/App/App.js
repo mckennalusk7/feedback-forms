b2a2da31c8c0581df9df067aace24ffe5bbe078e
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-17 16:01:36 -0600

    imported react-router-dom into app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
+          <h4>
+            <i>Don't forget it!</i>
+          </h4>
         </header>
-        <br/>
+        <br />
       </div>
     );
   }
