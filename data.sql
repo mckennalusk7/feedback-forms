1c01608e2da340d557141654b12ad4fcdb4c2668
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-17 15:37:01 -0600

    database created in SQL

--- data.sql
+++ data.sql
@@ -3,7 +3,8 @@ CREATE DATABASE "prime_feedback";
 
 -- Switch to "prime_feedback" before making:
 -- Table to store the feedback
-CREATE TABLE "feedback" (
+CREATE TABLE "feedback"
+(
   "id" serial primary key,
   "feeling" INT not null,
   "understanding" INT not null,
@@ -11,8 +12,10 @@ CREATE TABLE "feedback" (
   "comments" text,
   "flagged" boolean default false,
   "date" date not null default CURRENT_DATE
-); 
+);
 
 -- Sample feedback item
-INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
-VALUES (4, 4, 5, 'Doing Great!');
+INSERT INTO "feedback"
+  ("feeling", "understanding", "support", "comments")
+VALUES
+  (4, 4, 5, 'Doing Great!');
