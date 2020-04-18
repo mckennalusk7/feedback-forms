1f9dc07ef68870fc4edbd5437f254a3f04983ed7
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-18 14:00:07 -0600

    brought in all the reducers onto my review page to display the users input

--- src/components/Pages/ReviewFeedback/ReviewFeedback.js
+++ src/components/Pages/ReviewFeedback/ReviewFeedback.js
@@ -6,9 +6,18 @@ class ReviewFeedback extends Component {
     return (
       <div>
         <h2>Review of Feedback</h2>
+
+        <div>
+          <h4> {this.props.store.feelingReducer.feeling}</h4>
+          <h4> {this.props.store.contentReducer.content}</h4>
+          <h4> {this.props.store.supportReducer.support}</h4>
+          <h4> {this.props.store.commentReducer.comments}</h4>
+        </div>
       </div>
     );
   }
 }
 
-export default connect()(ReviewFeedback);
+const mapStoreToProps = (store) => ({ store });
+
+export default connect(mapStoreToProps)(ReviewFeedback);
