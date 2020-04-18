fde8fce2c3d6f0137f9886827b6bff0606eb803f
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-18 10:58:56 -0600

    imported all components into app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
+import FeelingPage from "../Pages/FeelingPage/FeelingPage";
+import ContentPage from "../Pages/ContentPage/ContentPage";
+import SupportedPage from "../Pages/SupportedPage/SupportedPage";
+import CommentsPage from "../Pages/CommentsPage/CommentsPage";
+import ReviewFeedback from "../Pages/ReviewFeedback/ReviewFeedback";
 
 class App extends Component {
   render() {
@@ -10,6 +15,12 @@ class App extends Component {
         <Router>
           <header className="App-header">
             <h1 className="App-title">Feedback!</h1>
+
+            <Route exact path="/" component={FeelingPage} />
+            <Route exact path="/content" component={ContentPage} />
+            <Route exact path="/supported" component={SupportedPage} />
+            <Route exact path="/comments" component={CommentsPage} />
+            <Route exact path="/review" component={ReviewFeedback} />
 
             <h4>
               <i>Don't forget it!</i>
