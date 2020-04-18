6fe105f83d401c2cf28c7f0c8f328e1ea4081953
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-04-18 11:45:36 -0600

    when information is entered, and user clicks next, information is dispatched and user is sent to next page! feelingpage complete

--- src/components/Pages/FeelingPage/FeelingPage.js
+++ src/components/Pages/FeelingPage/FeelingPage.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class FeelingPage extends Component {
+  onFeelingsClick = (type) => (event) => {
+    this.props.dispatch({ type: "SET_FEELINGS_TYPE", payload: type });
+    this.props.history.push("content");
+  };
+
   render() {
     return (
       <div>
         <h3> How are you Feeling?</h3>
 
-        <form>
+        <div>
           <input type="text"></input>
-          <button> Next </button>
-        </form>
+          <button onClick={this.onFeelingsClick("nextpage")}> Next </button>
+        </div>
       </div>
     );
   }
